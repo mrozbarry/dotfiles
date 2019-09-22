93d26936bf90f9af4d88a9e84f359154b07bb62a
Alex Barry <alex.barry@gmail.com>
2019-09-22 11:57:13 -0400

    Comment out osx-specific php settings

--- zsh/settings/php.zsh
+++ zsh/settings/php.zsh
@@ -3,5 +3,5 @@
 # export PATH="/usr/local/opt/php@7.1/sbin:$PATH"
 
 # For homebrew formula php@7.2
-echo 'export PATH="/usr/local/opt/php@7.2/bin:$PATH"' >> ~/.zshrc
-echo 'export PATH="/usr/local/opt/php@7.2/sbin:$PATH"' >> ~/.zshrc
+# export PATH="/usr/local/opt/php@7.2/bin:$PATH"
+# export PATH="/usr/local/opt/php@7.2/sbin:$PATH"
