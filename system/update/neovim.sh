125dff6ca523df65d4bdc8662cec4aaffe550b77
Alex Barry <alex.barry@gmail.com>
2020-02-22 14:38:55 -0500

    Add coc installer to neovim script

--- system/update/neovim.sh
+++ system/update/neovim.sh
@@ -1,3 +1,4 @@
 #/bin/bash
 
 nvim --headless +UpdateRemotePlugins +PlugUpgrade +PlugInstall +PlugUpdate +PlugClean +qall
+nvim --headless -c 'CocInstall -sync coc-json coc-php coc-tsserver coc-css coc-svg coc-eslint coc-html coc-inline-jest coc-tailwindcss|q'
