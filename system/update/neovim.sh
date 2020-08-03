6b2a6cc0d435abfc895b3517f503bd11f79afa23
Alex Barry <alex.barry@gmail.com>
2020-08-03 09:09:50 -0400

    Add coc flutter for neovim

--- system/update/neovim.sh
+++ system/update/neovim.sh
@@ -1,4 +1,4 @@
 #/bin/bash
 
 nvim --headless +UpdateRemotePlugins +PlugUpgrade +PlugInstall +PlugUpdate +PlugClean +qall
-nvim --headless -c 'CocInstall -sync coc-json coc-phpls coc-tsserver coc-css coc-eslint coc-html coc-tailwindcss|q'
+nvim --headless -c 'CocInstall -sync coc-json coc-phpls coc-tsserver coc-css coc-eslint coc-html coc-tailwindcss coc-flutter|q'
